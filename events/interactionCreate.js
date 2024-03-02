ee6b02845e7b126b1a16118aeddedd5c08c4ed62
fr1t2 <fr1t2@protonmail.com>
2024-03-02 15:29:40 -0700

    cooldown event fix

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -20,16 +20,19 @@ module.exports = {
 		const now = Date.now();
 		const timestamps = cooldowns.get(command.data.name);
 		const defaultCooldownDuration = 3;
-		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
 
 		if (timestamps.has(interaction.user.id)) {
 			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
 
 			if (now < expirationTime) {
-				const expiredTimestamp = Math.round(expirationTime / 1_000);
+				const expiredTimestamp = Math.round(expirationTime / 1000);
 				return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
 			}
 		}
+		
+		timestamps.set(interaction.user.id, now);
+		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
 		try {
 			await command.execute(interaction);

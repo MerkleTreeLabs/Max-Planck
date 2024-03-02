fb61c372767c900ffb6be66d4b82ebfc31c24770
fr1t2 <fr1t2@protonmail.com>
2024-03-02 16:33:02 -0700

    console

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -16,7 +16,7 @@ module.exports = {
 		if (!command) {
 			return interaction.reply(`There is no command with name \`${commandName}\`!`);
 		}
-
+		console.log(command.data.name)
 		delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)];
 
 		try {
--- index.js
+++ index.js
@@ -10,7 +10,6 @@ client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
-console.log(commandFolders);
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));

a78b4ff0b810d87d38f3b64a8f1f4e1fbb956406
fr1t2 <fr1t2@protonmail.com>
2024-03-03 09:52:16 -0700

    Remove old command

---
+++ modules/faucet.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const helper = require('./helpers');
+
+async function faucet(address, amount) {
+	try {
+		const response = await axios.post('http://127.0.0.1:8545', {
+			jsonrpc: '2.0',
+			method: 'zond_getBalance',
+			params: [address, 'latest'],
+			id: 1,
+		}, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		// format the balance to human readable
+		const hexString = response.data.result;
+		return await helper.hexToDec(hexString, denomination);
+	}
+	catch (error)	{
+        throw new Error(`Error occurred: ${error.message}`);
+	}
+}
+
+module.exports = balance;

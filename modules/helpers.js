32396f48c04fcecfae3191d479c566cfb9e688aa
fr1t2 <fr1t2@protonmail.com>
2024-03-03 01:50:41 -0700

    Console

--- modules/helpers.js
+++ modules/helpers.js
@@ -4,9 +4,12 @@ function sanitizeAddress(address) {
 }
 
 function validateAddress(address) {
+	console.log(`address given:\t${address}`)
     const zondAddressRegex = /^(0x)?[0-9a-f]{40}$/i;
     const sanitizedAddress = sanitizeAddress(address);
+    console.log(`sanitizedAddress:\t${sanitizedAddress}`)
     const lowercaseAddress = sanitizedAddress.toLowerCase();
+    console.log(`lowercaseAddress:\t${lowercaseAddress}`)
     if (lowercaseAddress.match(zondAddressRegex)) {
         return { isValid: true, address: lowercaseAddress };
     } else {

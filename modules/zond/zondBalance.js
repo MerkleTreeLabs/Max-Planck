c4e9e28c238c92bd887d3769ba437b58f2482da5
fr1t2 <fr1t2@protonmail.com>
2024-03-03 16:24:13 -0700

    Move balance to subfile

---
+++ modules/zond/zondBalance.js
@@ -0,0 +1,29 @@
+const getBalance = require('../api/balance');
+const helper = require('../helpers');
+
+
+async function getBalanceSub(interaction) {
+	const userAddress = interaction.options.getString('address');
+	try {
+		const validationResults = await helper.validateAddress(userAddress);
+		if (validationResults.isValid) {
+			let userBalance;
+			if (interaction.options.getString('denomination') === 'wei') {
+				userBalance = await getBalance(validationResults.address, 'wei');
+			}
+			else {
+				userBalance = await getBalance(validationResults.address, 'quanta');
+			}
+			await interaction.reply(`Balance info:\nAddress:\t\`${userAddress}\`\nBalance:\t\`${userBalance}\``);
+		}
+		else {
+			await interaction.reply(`Invalid address given:\t${validationResults.error}`);
+		}
+	}
+	catch (error) {
+		console.error('An error occurred during balance retrieval:', error);
+		await interaction.reply('Looks like I\'m struggling to complete that right now...');
+	}
+}
+
+module.exports = getBalanceSub;

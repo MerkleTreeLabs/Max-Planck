b385ed802ddcf2c844caa9764b339a133a25b75a
fr1t2 <fr1t2@protonmail.com>
2024-03-02 23:54:35 -0700

    axios example

--- modules/block.js
+++ modules/block.js
@@ -1,18 +1,23 @@
 const axios = require('axios');
 
-const response = await axios.post(
-	'http://127.0.0.1:8545',
-	'{"jsonrpc":"2.0",  \\ \n    "method":"zond_blockNumber",\\\n    "params":[], "id":1}',
-	{
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	},
-);
+async function block() {
+	try {
+		const response = await axios.post('http://127.0.0.1:8545', {
+			jsonrpc: '2.0',
+			method: 'zond_blockNumber',
+			params: [],
+			id: 1,
+		}, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		// format the block to human readable
+		return response.data;
+	}
+	catch (error)	{
+		throw new Error('Error occurred:', error);
+	}
+}
 
-module.exports = {
-	block: function() {
-		console.log(response);
-		return response;
-	},
-};
+module.exports = block;

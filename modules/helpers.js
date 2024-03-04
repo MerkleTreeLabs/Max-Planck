79267c683fa4928b7077f67e09284fdf424e2350
fr1t2 <fr1t2@protonmail.com>
2024-03-03 20:54:00 -0700

    Faucet function cleanup

--- modules/helpers.js
+++ modules/helpers.js
@@ -1,63 +1,187 @@
 const BigNumber = require('bignumber.js');
+const fs = require('fs');
+const userFile = '../userlog.json';
+
 
 function sanitizeAddress(address) {
-	const withoutPrefix = address.startsWith('0x') ? address.slice(2) : address;
-	const sanitized = withoutPrefix.replace(/[^0-9a-fA-F]/g, '');
-	return (address.startsWith('0x') ? '0x' : '') + sanitized;
+	try {
+		const withoutPrefix = address.startsWith('0x') ? address.slice(2) : address;
+		const sanitized = withoutPrefix.replace(/[^0-9a-fA-F]/g, '');
+		return (address.startsWith('0x') ? '0x' : '') + sanitized;
+	}
+	catch (error) {
+		console.error('Error occurred during address sanitization:', error);
+		return null;
+	}
 }
 
 function validateAddress(address) {
-	const zondAddressRegex = /^(0x)?[0-9a-f]{40}$/i;
-	const sanitizedAddress = sanitizeAddress(address);
-	const lowercaseAddress = sanitizedAddress.toLowerCase();
-	console.log(`validateAddress:\t${lowercaseAddress}`);
-	if (lowercaseAddress.match(zondAddressRegex)) {
-		return { isValid: true, address: lowercaseAddress };
+	try {
+		const zondAddressRegex = /^(0x)?[0-9a-f]{40}$/i;
+		const sanitizedAddress = sanitizeAddress(address);
+		const lowercaseAddress = sanitizedAddress.toLowerCase();
+		console.log(`validateAddress:\t${lowercaseAddress}`);
+		if (lowercaseAddress.match(zondAddressRegex)) {
+			return { isValid: true, address: lowercaseAddress };
+		}
+		else {
+			return { isValid: false, error: 'Invalid address' };
+		}
 	}
-	else {
-		return { isValid: false, error: 'Invalid address' };
+	catch (error) {
+		console.error('Error occurred during validation:', error);
+		return { isValid: false, error: 'Error validating given address' };
 	}
 }
 
 function validateTxHash(hash) {
-	const zondTxHashRegex = /^(0x)?[0-9a-f]{64}$/i;
-	const sanitizedTxHash = sanitizeAddress(hash);
-	const lowercaseTxHash = sanitizedTxHash.toLowerCase();
-	if (lowercaseTxHash.match(zondTxHashRegex)) {
-		return { isValid: true, hash: lowercaseTxHash };
+	try {
+		const zondTxHashRegex = /^(0x)?[0-9a-f]{64}$/i;
+		const sanitizedTxHash = sanitizeAddress(hash);
+		const lowercaseTxHash = sanitizedTxHash.toLowerCase();
+		if (lowercaseTxHash.match(zondTxHashRegex)) {
+			return { isValid: true, hash: lowercaseTxHash };
+		}
+		else {
+			return { isValid: false, error: 'Invalid TX Hash' };
+		}
 	}
-	else {
-		return { isValid: false, error: 'Invalid TX Hash' };
+	catch (error) {
+		console.error('Error occurred during validation:', error);
+		return { isValid: false, error: 'Error validating TX Hash' };
 	}
 }
 
 function hexToDec(value, denomination) {
-	const hexNumber = new BigNumber(value, 16);
-	if (!['quanta', 'wei'].includes(denomination)) {
-		throw new Error('Invalid denomination. Please provide "quanta" or "wei".');
+	try {
+		const hexNumber = new BigNumber(value, 16);
+		if (!['quanta', 'wei'].includes(denomination)) {
+			throw new Error('Invalid denomination. Please provide "quanta" or "wei".');
+		}
+		// this is working but not easy to understand
+		// return denomination === 'quanta' ? hexNumber.dividedBy('1e18').toFixed() : hexNumber.toFixed();
+		if (denomination === 'quanta') {
+			return hexNumber.dividedBy('1e18').toFixed();
+		}
+		else {
+			return hexNumber.toFixed();
+		}
 	}
-	return denomination === 'quanta' ? hexNumber.dividedBy('1e18').toFixed() : hexNumber.toFixed();
+	catch (error) {
+		console.error('Error occurred during conversion:', error);
+		return null;
+	}
 }
 
 
 function quantaToShor(number) {
-	const bigNumber = new BigNumber(number);
-	const multiplied = bigNumber.multipliedBy('1e18');
-	const resultString = multiplied.toFixed();
-	const result = resultString.replace('.', '');
-	console.log(result);
-	return result;
+	try {
+		const bigNumber = new BigNumber(number);
+		const multiplied = bigNumber.multipliedBy('1e18');
+		const resultString = multiplied.toFixed(0);
+		const result = resultString.replace('.', '');
+		console.log(result);
+		return result;
+	}
+	catch (error) {
+		console.error('Error occurred during conversion:', error);
+		return null;
+	}
 }
 
 
 function shorToQuanta(number) {
-    const bigNumber = new BigNumber(number);
-    const divided = bigNumber.dividedBy('1e18');
-    const result = divided.toFixed();
-    console.log(result);
-    return result;
+	try {
+		const bigNumber = new BigNumber(number);
+		const divided = bigNumber.dividedBy('1e18');
+		const result = divided.toFixed();
+		return result;
+	}
+	catch (error) {
+		console.error('Error occurred during conversion:', error);
+		return null;
+	}
 }
 
+
+function userLookup(userInfo) {
+	try {
+		// read and parse the userlog.json file
+		const userData = fs.readFileSync(userFile);
+		const parsedData = JSON.parse(userData);
+		// Find user information by discord_id.
+		const foundUserIndex = parsedData.users.findIndex(user => user.discordId === userInfo.discordId);
+		if (foundUserIndex !== -1) {
+			// user is found
+			return { isFound: true, data: parsedData.users[foundUserIndex] };
+		}
+		else {
+			// not found, return error
+			return { isFound: false, error: 'User not found' };
+		}
+	}
+	catch (error) {
+		// handle errors
+		return { isFound: false, error: error.message };
+	}
+}
+
+
+function formatTime(milliseconds) {
+	// Convert milliseconds to seconds
+	const totalSeconds = Math.floor(milliseconds / 1000);
+
+	// Calculate hours, minutes, and seconds
+	const hours = Math.floor(totalSeconds / 3600);
+	const minutes = Math.floor((totalSeconds % 3600) / 60);
+	const seconds = totalSeconds % 60;
+
+	// Build the formatted time string
+	let formattedTime = '';
+	if (hours > 0) {
+		formattedTime += `${hours} hour${hours > 1 ? 's' : ''} `;
+	}
+	if (minutes > 0 || hours > 0) {
+		formattedTime += `${minutes} min `;
+	}
+	formattedTime += `${seconds} sec{seconds !== 1 ? 's' : ''}`;
+
+	return formattedTime;
+}
+
+
+function writeUserData(newData) {
+	try {
+		// Read the userlog.json file
+		const userData = fs.readFileSync(userFile);
+		const parsedData = JSON.parse(userData);
+
+		// Find user information by discord_id
+		const userIndex = parsedData.users.findIndex(user => user.discordId === newData.discordId);
+
+		if (userIndex !== -1) {
+			// Update existing user data
+			parsedData.users[userIndex] = newData;
+		}
+		else {
+			// Append new user data
+			parsedData.users.push(newData);
+		}
+
+		// Write the updated data back to the file
+		fs.writeFileSync(userFile, JSON.stringify(parsedData, null, 2));
+
+		return 'Success';
+	}
+	catch (error) {
+		console.error('Error occurred during file operation:', error);
+		return 'Fail';
+	}
+}
+
+exports.writeUserData = writeUserData;
+exports.formatTime = formatTime;
+exports.userLookup = userLookup;
 exports.shorToQuanta = shorToQuanta;
 exports.quantaToShor = quantaToShor;
 exports.validateAddress = validateAddress;

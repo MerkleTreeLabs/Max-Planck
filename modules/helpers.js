b2bd9e0c7dd6d4563ba6ba742160a0de22c6b51d
fr1t2 <fr1t2@protonmail.com>
2024-03-03 22:49:04 -0700

    Console

--- modules/helpers.js
+++ modules/helpers.js
@@ -114,6 +114,7 @@ function userLookup(userInfo) {
 		const parsedData = JSON.parse(userData);
 		// Find user information by discord_id.
 		const foundUserIndex = parsedData.users.findIndex(user => user.discordId === userInfo.discordId);
+		console.log(`foundUserIndex:\t ${foundUserIndex}`)
 		if (foundUserIndex !== -1) {
 		console.log(`FOUND!\nData: \t ${JSON.parse(parsedData.users[foundUserIndex])}`)
 			// user is found

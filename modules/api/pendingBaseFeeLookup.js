5a715888a3dc764efa75a9a03c58785547f081a5
fr1t2 <fr1t2@protonmail.com>
2024-03-07 22:52:05 -0700

    pendingbasefee

---
+++ modules/api/pendingBaseFeeLookup.js
@@ -0,0 +1,27 @@
+const config = require('../../config.json');
+const axios = require('axios');
+
+
+async function getPendingBaseFee() {
+	try {
+		const pendingBaseFee = await axios.post(`http://${config.zondPubAPI}`, {
+			jsonrpc: '2.0',
+			method: 'zond_getMaxPriorityFeePerGas',
+			params: [],
+			id: 1,
+		}, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		return pendingBaseFee.data.result;
+	}
+	catch (error) {
+		console.log(`error caught: ${error}`);
+	}
+}
+
+module.exports = pendingBaseFee;
+
+
+// const pendingBaseFee = (await axios.get(`${config.zondPubAPI}/pendingBaseFee`)).data.result;

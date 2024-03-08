7b310483d8a0d9c266c7fc6c6bee42f09d71a5f4
fr1t2 <fr1t2@protonmail.com>
2024-03-07 22:02:27 -0700

    correct call

--- modules/api/faucet.js
+++ modules/api/faucet.js
@@ -19,7 +19,7 @@ async function sendFaucetTx(toAddress, amount) {
 
 		const nonce = await axios.post(`http://${config.zondPubAPI}`, {
 			jsonrpc: '2.0',
-			method: 'zond_nonce',
+			method: 'getTransactionCount',
 			params: [`0x${toAddress}`, 'latest'],
 			id: 1,
 		}, {

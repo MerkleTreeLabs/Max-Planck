65f1c9a5edf8b8b07f908e3d92dcae720d7277d2
fr1t2 <fr1t2@protonmail.com>
2024-03-21 13:12:37 -0600

    cleanup console.lgo

--- modules/zond/zondBalance.js
+++ modules/zond/zondBalance.js
@@ -9,25 +9,31 @@ async function getBalanceSub(interaction) {
 		const validationResults = await helper.validateAddress(userAddress);
 		if (validationResults.isValid) {
 			let userBalance;
-			if (interaction.options.getString('denomination') === 'wei') {
-				userBalance = await getBalance(validationResults.address);
-			}
-			else {
-				userBalance = await getBalance(validationResults.address);
-				console.log(userBalance);
-				// userBalance = userBalance.dividedBy('1e18').toFixed();
-			}
-			await interaction.reply(`Balance info:\nAddress:\t\`${userAddress}\`\nBalance:\t\`${userBalance}\``);
-		}
-		else {
-			await interaction.reply(`Invalid address given:\t${validationResults.error}`);
-		}
-	}
-	catch (error) {
-		const errorMessage = `An error occurred during balance retrieval: ${error.message}`;
-		console.error(errorMessage);
-		return await interaction.reply(`Looks like I'm struggling to complete that right now...\n${errorMessage}`);
-	}
+
+            // Get the balance from the API
+            const balanceResponse = await getBalance(validationResults.address);
+            const balanceValue = balanceResponse.toString(); // Convert the balance to string
+
+
+            // Convert the balance value to BigNumber
+            userBalance = new BigNumber(balanceValue);
+
+            // If the denomination is not wei, divide the balance by 1e18
+            if (interaction.options.getString('denomination') !== 'wei') {
+                userBalance = userBalance.dividedBy('1e18').toFixed();
+            }
+
+            // Reply with the balance information
+            await interaction.reply(`Balance info:\nAddress:\t\`${userAddress}\`\nBalance:\t\`${userBalance}\``);
+        } else {
+            await interaction.reply(`Invalid address given:\t${validationResults.error}`);
+        }
+    } catch (error) {
+        const errorMessage = `An error occurred during balance retrieval: ${error.message}`;
+        console.error(errorMessage);
+        await interaction.reply(`Looks like I'm struggling to complete that right now...\n${errorMessage}`);
+    }
 }
+
 
 module.exports = getBalanceSub;

aab525649fe3df2479fb51a5f75c514d5f9160f8
fr1t2 <fr1t2@protonmail.com>
2024-03-28 02:12:37 -0600

    feat(api): add additional api funcitons

--- src/modules/zond/zondBlock.js
+++ src/modules/zond/zondBlock.js
@@ -21,3 +21,33 @@ async function getBlockSub(interaction) {
 }
 
 module.exports = getBlockSub;
+
+/*
+// using the new api
+const axios = require('axios');
+const { apiPort } = require('../../config.json');
+const PORT = apiPort || 3000
+
+async function getBlockSub(interaction) {
+	try {
+		// Fetch the current block number from the API
+		const response = await axios.get(`http://localhost:${PORT}/zond-block`);
+		const blockNumber = response.data.currentBlock.number;
+
+		// Return the block number
+		if (blockNumber) {
+			return await interaction.reply(`Latest Zond Block:\t\`${blockNumber}\``);
+		} else {
+			return await interaction.reply(`Cannot retrieve the BlockNumber at this time...\n\`${blockNumber}\``);
+		}
+	} catch (error) {
+		// Handle the error
+		const errorMessage = `Error occurred while fetching the block number: ${error.message}`;
+		console.error(errorMessage);
+		return await interaction.reply(`Looks like I'm struggling to complete that right now...\n\`${errorMessage}\``);
+	}
+}
+
+module.exports = getBlockSub;
+
+*/
